06105e1420f57e22fc4e4035ca1c6f13b1aab261
accountASTR <account@theastr.com>
2024-10-12 14:47:43 +0000

    Fix CORS issue

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://adminaztrproject-0ba7d5b16bef.herokuapp.com'],
+    'allowed_origins' => ['https://adminaztrproject-b332c3269318.herokuapp.com/'],
 
     'allowed_origins_patterns' => [],
 
